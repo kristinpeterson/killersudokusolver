25176dd098e31905189d2019ab7480cf29ab3f1e
kristinpeterson <kristinpeterson@me.com>
2015-03-07 18:14:57 -0800

    Implement sumCombinations method which finds all possible solutions for each Cage.  Note: Solution permutations not yet implemented.

--- Cage.java
+++ Cage.java
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 public class Cage{
 
 	private int goal;
-	private ArrayList cells;
+	private ArrayList<Cell> cells;
 
 	public Cage(String[] info){
-		cells = new ArrayList();
-		for (int i=0; i<info.length; i++) {
-			if(i == info.length -1 ){
+		cells = new ArrayList<Cell>();
+		for (int i=0; i < info.length; i++) {
+			if(i == info.length - 1 ){
 				goal = Integer.parseInt(info[i]);
 			} else {
 				cells.add(new Cell(info[i], info[i+1]));
@@ -22,9 +22,17 @@ public class Cage{
 		}
 	}
 
+	public int getGoal()  {
+		return goal;
+	}
+
+	public ArrayList getCells() {
+		return cells;
+	}
+
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
-		for (int i=0; i<cells.size(); i++) {
+		for (int i=0; i < cells.size(); i++) {
 			Cell t = (Cell)cells.get(i);
 			sb.append(t.getX());
 			sb.append(",");
--- KillerSudoku.java
+++ KillerSudoku.java
@@ -7,23 +7,27 @@ import java.io.FileReader;
 import java.io.File;
 import java.io.BufferedReader;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class KillerSudoku{
-	
-	private ArrayList cages;
+
+	private ArrayList<Cage> cages;
+
+	// These vars are used for finding possible sum combinations for all Cages
+	private static int[] possibleValues = {0,1,2,3,4,5,6,7,8,9};
+	private Stack<Integer> stack = new Stack<Integer>();
+	private int sum = 0;
 
 	public KillerSudoku(){
-		cages = new ArrayList();
-	}
-
-	public void addCage(Cage c){
-		cages.add(c);
+		cages = new ArrayList<Cage>();
 	}
 
 	public static void main(String[] argv) throws Exception {
 
 		KillerSudoku ks = new KillerSudoku();
 
+		/** Get cages from file */
 	    File f = new File("test.txt");
     	FileReader fr = new FileReader(f);
     	BufferedReader br = new BufferedReader(fr);
@@ -35,7 +39,54 @@ public class KillerSudoku{
     		ks.addCage(new Cage(eachLine.split(",")));
       		eachLine = br.readLine();
     	}
-    	System.out.println("Done");
+
+		/** Find all combinations that add up to each Cage goal */
+		ArrayList<List<Stack<Integer>>> sumCombinationsPerCage = new ArrayList<List<Stack<Integer>>>();
+		for(int i = 0; i < ks.cages.size(); i++) {
+			int goal = ks.cages.get(i).getGoal();
+			int size = ks.cages.get(i).getCells().size();
+			List<Stack<Integer>> possibleSolutions = new ArrayList<Stack<Integer>>();
+			sumCombinationsPerCage.add(ks.sumCombinations(0, 9, goal, size, possibleSolutions));
+		}
+
+		// Printing possible solutions for Cages
+		for(int i = 0; i < sumCombinationsPerCage.size(); i++) {
+			System.out.println(sumCombinationsPerCage.get(i).toString());
+		}
+
+		// TODO: Permutations of possible solutions
+
+	}
+
+	private void addCage(Cage c){
+		cages.add(c);
+	}
+
+	private List<Stack<Integer>> sumCombinations(int fromIndex, int endIndex, int target, int size, List<Stack<Integer>> possibleSolutions) {
+
+		if (sum == target && stack.size() == size) {
+			// possible solution found!
+			Stack<Integer> possibleSolution = new Stack<Integer>();
+			// Make copy of stack, reason:
+			// cannot directly add stack to possibleSolutions list
+			// because any changes to stack will propogate to possibleSolutions
+			// list, rendering it useless.
+			for(int i = 0; i < stack.size(); i++) {
+				possibleSolution.add(stack.get(i));
+			}
+			possibleSolutions.add(possibleSolution);
+		}
+
+		for (int currentIndex = fromIndex; currentIndex < endIndex; currentIndex++) {
+			if (sum + possibleValues[currentIndex] <= target) {
+				stack.push(possibleValues[currentIndex]);
+				sum += possibleValues[currentIndex];
+				sumCombinations(currentIndex + 1, endIndex, target, size, possibleSolutions);
+				sum -= stack.pop();
+			}
+		}
+
+		return possibleSolutions;
 	}
 
 }
