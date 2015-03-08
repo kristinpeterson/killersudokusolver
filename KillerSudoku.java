2054a688f14ce7ce83dde564258c5bfd13b26510
kristinpeterson <kristinpeterson@me.com>
2015-03-07 18:58:55 -0800

    Fix bug in sumCombinations

--- KillerSudoku.java
+++ KillerSudoku.java
@@ -16,8 +16,6 @@ public class KillerSudoku{
 
 	// These vars are used for finding possible sum combinations for all Cages
 	private static int[] possibleValues = {0,1,2,3,4,5,6,7,8,9};
-	private Stack<Integer> stack = new Stack<Integer>();
-	private int sum = 0;
 
 	public KillerSudoku(){
 		cages = new ArrayList<Cage>();
@@ -46,11 +44,15 @@ public class KillerSudoku{
 			int goal = ks.cages.get(i).getGoal();
 			int size = ks.cages.get(i).getCells().size();
 			List<Stack<Integer>> possibleSolutions = new ArrayList<Stack<Integer>>();
-			sumCombinationsPerCage.add(ks.sumCombinations(0, 9, goal, size, possibleSolutions));
+			int sum = 0;
+			Stack<Integer> stack = new Stack<Integer>();
+			sumCombinationsPerCage.add(ks.sumCombinations(stack, sum, 0, possibleValues.length, goal, size, possibleSolutions));
 		}
 
 		// Printing possible solutions for Cages
-		for(int i = 0; i < sumCombinationsPerCage.size(); i++) {
+		for(int i = 0; i < ks.cages.size(); i++) {
+			System.out.print("Cage size: " + ks.cages.get(i).getCells().size() +
+					"\t| Cage Goal: " + ks.cages.get(i).getGoal() + "\t|\t");
 			System.out.println(sumCombinationsPerCage.get(i).toString());
 		}
 
@@ -62,14 +64,15 @@ public class KillerSudoku{
 		cages.add(c);
 	}
 
-	private List<Stack<Integer>> sumCombinations(int fromIndex, int endIndex, int target, int size, List<Stack<Integer>> possibleSolutions) {
+	private List<Stack<Integer>> sumCombinations(Stack<Integer> stack, int sum, int fromIndex, int endIndex,
+												 int target, int size, List<Stack<Integer>> possibleSolutions) {
 
 		if (sum == target && stack.size() == size) {
 			// possible solution found!
 			Stack<Integer> possibleSolution = new Stack<Integer>();
 			// Make copy of stack, reason:
 			// cannot directly add stack to possibleSolutions list
-			// because any changes to stack will propogate to possibleSolutions
+			// because any changes to stack will propagate to possibleSolutions
 			// list, rendering it useless.
 			for(int i = 0; i < stack.size(); i++) {
 				possibleSolution.add(stack.get(i));
@@ -81,7 +84,7 @@ public class KillerSudoku{
 			if (sum + possibleValues[currentIndex] <= target) {
 				stack.push(possibleValues[currentIndex]);
 				sum += possibleValues[currentIndex];
-				sumCombinations(currentIndex + 1, endIndex, target, size, possibleSolutions);
+				sumCombinations(stack, sum, currentIndex + 1, endIndex, target, size, possibleSolutions);
 				sum -= stack.pop();
 			}
 		}
